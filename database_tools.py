4ae22ce1bd4c07d1cca9986f458a57abaf3d776e
Hampus Björklin <hampus.bjorklin@hotmail.com>
2023-03-02 14:45:19 +0100

    Test

--- database_tools.py
+++ database_tools.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def initialize_database(name='Quotes'):
     # Initialize database with single table containing basic info about securities used for scraping
-    path = 'C:/VSCode/project-saturday/'
+    path = './'
     if name[-3:] != '.db':
         name = name + '.db'
     db_conn = sqlite3.connect(path+name)
@@ -13,6 +13,7 @@ def initialize_database(name='Quotes'):
     sql = """CREATE TABLE IF NOT EXISTS TickerInfo(
     NAME VARCHAR(40) NOT NULL,
     ISIN VARCHAR(20) PRIMARY KEY,
+    TYPE VARCHAR(30) NOT NULL, 
     COUNTRY VARCHAR(20) NOT NULL,
     AVANZA_ID INTEGER
     )"""
@@ -25,23 +26,26 @@ def insert_security(db_cursor, record):
     # Insert security to inital table with scraping information such as the securities Avanza-specific ID.
     record = [r.strip() if isinstance(r, str) else r for r in record]
     checksum = 0
-    if len(record) != 4:
+    if len(record) != 5:
         print('Invalid insertion: Wrong number of elements')
         sys.exit(1)
     if isinstance(record[0], str):
         checksum += 1
-    if isinstance(record[1], str):
+    if isinstance(record[1], str) and len(record[1]) == 12:
+        checksum += 1
+    if isinstance(record[2], str):
         checksum += 1
     if isinstance(record[3], str) and len(record[3])==2:
         checksum += 1
-    if isinstance(record[1], int):
+    if isinstance(record[4], int):
         checksum += 1
     
-    if checksum == 4:
+    if checksum == 5:
         sql = f"""INSERT OR REPLACE INTO TickerInfo 
-                  (ID, NAME, ISIN, COUNTRY) 
-                  VALUES({record[0]}, "{record[1]}", "{record[2]}", "{record[3]}");"""
+                  (NAME, ISIN, TYPE, COUNTRY, AVANZA_ID) 
+                  VALUES("{record[0]}", "{record[1]}", "{record[2].lower().replace(' ', '-')}", "{record[3]}", {record[4]});"""
         db_cursor.execute(sql)
+        print('Succesfully added security!')
     else:
         print('Invalid insertion: Wrong datatype(s)')
         sys.exit(1)
@@ -49,4 +53,10 @@ def insert_security(db_cursor, record):
 def initialize_quote_table(db_cursor):
     pass
 
-initialize_database()
+db_conn = sqlite3.connect('Quotes.db')
+db_cursor = db_conn.cursor()
+
+#initialize_database()
+insert_security(db_cursor, ['OMX STOCKHOLM PI', 'SE0000744195', 'market-index', 'SE', 18988])
+db_conn.commit()
+db_conn.close()
